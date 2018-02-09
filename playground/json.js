4caf776509e97c82dc1b9d77ddfb9a01bae39b6b
David Thorpe <davidthorpe878@gmail.com>
2018-02-09 21:36:42 +0000

    ried a few code tests re: JSON in playground folder

--- playground/json.js
+++ playground/json.js
@@ -1,8 +1,30 @@
-var obj = {
-    name: 'David'
+// var obj = {
+//     name: 'David'
+// };
+
+// var stringObj = JSON.stringify(obj);
+
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "David", "age": 35}';
+
+// var person = JSON.parse(personString);
+
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+    title: 'SOme tirle',
+    body: 'some  bodsy'
 };
 
-var stringObj = JSON.stringify(obj);
+const originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
 
-console.log(typeof stringObj);
-console.log(stringObj);
+var noteString = fs.readFileSync('notes.json');
+const note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
